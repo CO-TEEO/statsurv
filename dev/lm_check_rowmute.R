399c805e9857e0a3e826a5c8f6b68dab6ca7ab48
promerpr <69762060+promerpr@users.noreply.github.com>
2022-01-25 10:52:02 -0700

    Adding lm check

--- dev/lm_check_rowmute.R
+++ dev/lm_check_rowmute.R
@@ -1,57 +1,37 @@
 library(tidyverse)
+library(progressr)
 
 # We're going to set up a very basic model and then try to break it
-space_coord <- data.frame(space_label = c("space1", "space2", "space3"),
-                          stringsAsFactors = FALSE) %>%
-  gridcoord::gc_gridcoord()
+spacetime_data <- expand.grid(id_time = 1:12, id_space = 1:3) %>%
+  mutate(time_labels = paste0("X", id_time),
+         t = id_time,
+         y = t * 1.05 + rnorm(n = max(row_number())))
 
-
-
-start_times <- 1:12
-fin_times <- start_times + 1L
-labels <- paste0("X", start_times) #sapply(start_times + 64, intToUtf8)
-time_coord <- data.frame(time_labels = labels,
-                         time = start_times,
-                         fin_time = fin_times,
-                         stringsAsFactors = FALSE) %>%
-  gridcoord::gc_gridcoord()
-data_for_model <- gridcoord::gc_expand(time_coord, space_coord)
-data_for_model$y <- data_for_model$time * 1.05 + rnorm(n = nrow(data_for_model))
-
-spacetime_data <- data_for_model %>%
-  mutate(id_space = as.numeric(factor(space_label)),
-         id_time = time) %>%
-  as_tibble() %>%
-  select(id_space, id_time, time, fin_time, y)
-
-spacetime_data$id_time
 
 # That works, but feels a little...fragile
 # I feel like I would need to put in a way to make sure that the index is complete and ordered
 # But otherwise, that does the major windowing that I want to do
 simple_lm_func <- function(data_for_model) {
   Sys.sleep(1)
-  fit <- lm(y ~ time,
+  fit <- lm(y ~ t,
             data = data_for_model)
   return(fit)
 }
-library(progressr)
+
 handlers(global = TRUE)
+windowed_data <- window_idtime(spacetime_data, min_train = 7, max_train = 10,
+                               n_predict = 1, model_arity = "uni")
+prepped_data <- windowed_data %>%
+  rowmute(input_data = prepare_prediction_data(curr_data, y, split_id = split_id, prep_strategy = "NA"))
 
-lm_res <- loop_model(spacetime_data,
-           "y",
-           simple_lm_func,
-           data_prep_function = NULL,
-           min_train = 7,
-           max_train = 10,
-           n_predict = 1,
-           model_arity = "uni",
-           prediction_strategy = "NA")
+lm_res <- prepped_data %>%
+  rowmute(fits = simple_lm_func(input_data))
 
+lm_yhat <- lm_res %>%
+  rowmute(aug_data = extract_yhat(fits, curr_data))
 
-lm_yhat <- loop_extract_yhat(lm_res,"extract",  NULL, include_surveillance = TRUE)
-lm_yhat$augmented_data
+collapsed_data <- collapse_if_exploded(lm_yhat)
 
-lm_yhat$surveillance_data
+alarm_res <- collapsed_data %>%
+  rowmute(alarm = parallel_cusum_gaussian2(aug_data, "y", ".fitted"))
 
-loop_alarm_function(lm_yhat, "y", "parallel_cusum_gaussian")
