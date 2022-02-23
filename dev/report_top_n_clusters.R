3bd71baae900f89f5aa6bc26ffa317fd4131af00
promerpr <69762060+promerpr@users.noreply.github.com>
2022-02-22 17:45:37 -0700

    Remove unnecessary files

--- dev/report_top_n_clusters.R
+++
@@ -1,6 +0,0 @@
-report_top_n_clusters <- function(scnsttst, top_n = 5, allow_overlapping_clusters) {
-  # I want zone_number, locations, duration, score, relative_risk, and action_level
-  # That's a bigger project than I can do right now.
-  scnsttst$observed %>%
-    dplyr::slice_max(score, n = top_n, with_ties = FALSE)
-}
